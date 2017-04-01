9ae8fc984be4b6b4a0ebcb2d22007fd9d43fb5ee
qt4cpp <qt4cpp@gmail.com>
2017-04-02 07:57:57 +0900

    ファイルを読み込んで、ファイルパスをリストとして、表示はファイル名のみにした。

--- playlist.py
+++ playlist.py
@@ -1,1 +1,40 @@
-from PyQt5.Widgets import QListView, QFileDialog
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import (QApplication, QListWidget, QFileDialog, QPushButton, QHBoxLayout, QVBoxLayout, QWidget,
+                             QStyle)
+from PyQt5.QtMultimedia import QMediaContent, QMediaResource
+
+
+class Playlist(QWidget):
+
+    def __init__(self, parent=None):
+        super(Playlist, self).__init__(parent)
+
+        self.m_playlist = []
+        self.playListView = QListWidget()
+        self.openButton = QPushButton('open')
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.playListView)
+        layout.addWidget(self.openButton)
+        self.setLayout(layout)
+
+        self.openButton.clicked.connect(self.open)
+
+        self.show()
+
+
+    def open(self):
+        fileURL, _ = QFileDialog.getOpenFileUrl(self, 'Open File')
+
+        if not fileURL.isEmpty():
+            self.m_playlist.append(fileURL)
+            print(fileURL)
+            self.playListView.addItem(fileURL.fileName())
+
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+
+    playlist = Playlist()
+    sys.exit(app.exec_())
