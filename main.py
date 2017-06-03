2f42746b4b90ca028a75c7f3c2756eaa56d0e02f
Ikemoto Tatsuhiko <qt4cpp@gmail.com>
2017-06-04 07:27:03 +0900

    split PypePlayer class from player.py file.

--- main.py
+++ main.py
@@ -1,3 +1,63 @@
-pass
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
 
-## Todo Pype_playerクラスを後で持ってくる。
+from player import Player
+from utility import createAction
+
+
+class PypePlayer(QMainWindow):
+
+    def __init__(self, parent=None):
+        super(PypePlayer, self).__init__(parent)
+
+        player = Player()
+        self.setCentralWidget(player)
+        self.createMenus(player)
+
+        self.resize(600, 360)
+        self.setWindowTitle('Pype Player')
+        self.show()
+
+
+    def createMenus(self, player):
+        openFile = createAction(self, 'Open', player.open, 'Ctrl+o')
+
+        add_playlist = createAction(self, 'Add playlist', player.playlist_tab.add_playlist, 'Ctrl+N')
+        rename_playlist = createAction(self, 'Rename Playlist', player.playlist_tab.rename_playlist)
+        remove_playlist = createAction(self, 'Remove Playlist', player.playlist_tab.remove_playlist)
+
+        forward_short = createAction(self, 'Short Forward', player.forward_short, 'Right')
+        forward_medium = createAction(self, 'Forward', player.forward_medium, 'Shift+Right')
+        forward_long = createAction(self, 'Long Forward', player.forward_long, 'Ctrl+Right')
+        forward_verylong = createAction(self, 'Very Long Forward', player.forward_verylong, 'Shift+Ctrl+Right')
+        backward_short = createAction(self, 'Short Backward', player.backward_short, 'Left')
+        backward_medium = createAction(self, 'Backward', player.backward_medium, 'Shift+Left')
+        backward_long = createAction(self, 'Long Backward', player.backward_long, 'Ctrl+Left')
+        backward_verylong = createAction(self, 'Very Long Backward', player.backward_verylong, 'Shift+Ctrl+Left')
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(openFile)
+        fileMenu.addSeparator()
+        fileMenu.addAction(add_playlist)
+        fileMenu.addAction(rename_playlist)
+        fileMenu.addAction(remove_playlist)
+
+        playbackMenu = menubar.addMenu('&Playback')
+        jumpMenu = playbackMenu.addMenu('Jump')
+        jumpMenu.addAction(forward_short)
+        jumpMenu.addAction(forward_medium)
+        jumpMenu.addAction(forward_long)
+        jumpMenu.addAction(forward_verylong)
+        jumpMenu.addSeparator()
+        jumpMenu.addAction(backward_short)
+        jumpMenu.addAction(backward_medium)
+        jumpMenu.addAction(backward_long)
+        jumpMenu.addAction(backward_verylong)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    pypePlayer = PypePlayer()
+    sys.exit(app.exec_())
--- player.py
+++ player.py
@@ -1,16 +1,13 @@
-import sys
 from enum import IntEnum
 
 from PyQt5.QtCore import (QUrl, Qt, QTime, QTimer, pyqtSignal)
 from PyQt5.QtGui import (QPalette)
 from PyQt5.QtMultimedia import (QMediaContent, QMediaPlayer)
 from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtWidgets import (QMainWindow, QApplication, QWidget, QLabel,
-                             QHBoxLayout, QVBoxLayout, QSizePolicy, QPushButton, QStyle,
-                             QSlider)
+from PyQt5.QtWidgets import (QWidget, QLabel, QHBoxLayout, QVBoxLayout, QSizePolicy, QPushButton,
+                             QStyle, QSlider)
 
 from playlisttab import PlaylistTab
-from utility import createAction
 
 
 class SeekStep(IntEnum):
@@ -354,60 +351,3 @@ class Player(QWidget):
     def backward_verylong(self):
         self.backward(SeekStep.VERYLONG)
 
-
-class PypePlayer(QMainWindow):
-
-    def __init__(self, parent=None):
-        super(PypePlayer, self).__init__(parent)
-
-        player = Player()
-        self.setCentralWidget(player)
-        self.createMenus(player)
-
-        self.resize(600, 360)
-        self.setWindowTitle('Pype Player')
-        self.show()
-
-
-    def createMenus(self, player):
-        openFile = createAction(self, 'Open', player.open, 'Ctrl+o')
-
-        add_playlist = createAction(self, 'Add playlist', player.playlist_tab.add_playlist, 'Ctrl+N')
-        rename_playlist = createAction(self, 'Rename Playlist', player.playlist_tab.rename_playlist)
-        remove_playlist = createAction(self, 'Remove Playlist', player.playlist_tab.remove_playlist)
-
-        forward_short = createAction(self, 'Short Forward', player.forward_short, 'Right')
-        forward_medium = createAction(self, 'Forward', player.forward_medium, 'Shift+Right')
-        forward_long = createAction(self, 'Long Forward', player.forward_long, 'Ctrl+Right')
-        forward_verylong = createAction(self, 'Very Long Forward', player.forward_verylong, 'Shift+Ctrl+Right')
-        backward_short = createAction(self, 'Short Backward', player.backward_short, 'Left')
-        backward_medium = createAction(self, 'Backward', player.backward_medium, 'Shift+Left')
-        backward_long = createAction(self, 'Long Backward', player.backward_long, 'Ctrl+Left')
-        backward_verylong = createAction(self, 'Very Long Backward', player.backward_verylong, 'Shift+Ctrl+Left')
-
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(openFile)
-        fileMenu.addSeparator()
-        fileMenu.addAction(add_playlist)
-        fileMenu.addAction(rename_playlist)
-        fileMenu.addAction(remove_playlist)
-
-        playbackMenu = menubar.addMenu('&Playback')
-        jumpMenu = playbackMenu.addMenu('Jump')
-        jumpMenu.addAction(forward_short)
-        jumpMenu.addAction(forward_medium)
-        jumpMenu.addAction(forward_long)
-        jumpMenu.addAction(forward_verylong)
-        jumpMenu.addSeparator()
-        jumpMenu.addAction(backward_short)
-        jumpMenu.addAction(backward_medium)
-        jumpMenu.addAction(backward_long)
-        jumpMenu.addAction(backward_verylong)
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-
-    pypePlayer = PypePlayer()
-    sys.exit(app.exec_())
