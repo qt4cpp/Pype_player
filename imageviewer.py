8dbc9bcc0c3f9aedc53a98796698b3fd8d37393e
qt4cpp <qt4cpp@gmail.com>
2017-11-03 22:33:30 +0900

    add: resize that keeping aspect ratio. and cleaning code.

--- imageviewer.py
+++ imageviewer.py
@@ -8,28 +8,31 @@ class ImageViewer(QLabel):
         super().__init__(parent)
         self.setScaledContents(True)
 
-        pixmap = self.load_image('test/image/duo-270.png')
-        self.setPixmap(pixmap)
-        print(pixmap.height(), pixmap.width())
-        print(self.aspect_ratio(pixmap))
-
-        self.resize(400, 500)
-        self.show()
-
     def load_image(self, path='', ):
         # check file exists
         pixmap = QPixmap()
         pixmap.load(path)
+        self.setPixmap(pixmap)
         return pixmap
 
-    def aspect_ratio(self, pixmap: QPixmap):
+    def resize_keep_aspect_ratio(self, base_size=500):
+        pixmap = self.pixmap()
         height = pixmap.height()
         width = pixmap.width()
+        if height > width:
+            self.resize(base_size, base_size*self.aspect_ratio(height, width))
+        else:
+            self.resize(base_size*self.aspect_ratio(height, width), base_size)
+
+    def aspect_ratio(self, height, width):
         return max(height, width) / min(height, width)
 
 if __name__ == '__main__':
     import sys
     app = QApplication(sys.argv)
 
-    playlist = ImageViewer()
+    image_viewer = ImageViewer()
+    image_viewer.load_image('test/image/duo-270.png')
+    image_viewer.resize_keep_aspect_ratio(base_size=600)
+    image_viewer.show()
     sys.exit(app.exec_())
